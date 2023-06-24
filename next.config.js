d38344ae529356c1aef07697cde9c8c7358e54db
Maurício Braz Gonçalves <82123955+mauriciobraz@users.noreply.github.com>
2023-06-24 19:02:49 -0400

    feat(core): configure `@svgr/webpack`

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* ... */
+  webpack: (config) => {
+    const fileLoaderRule = config.module.rules.find((rule) => {
+      return rule.test?.test?.('.svg');
+    });
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        resourceQuery: /url/,
+        test: /\.svg$/i,
+      },
+      {
+        use: ['@svgr/webpack'],
+        resourceQuery: { not: /url/ },
+        issuer: /\.[jt]sx?$/,
+        test: /\.svg$/i,
+      }
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
