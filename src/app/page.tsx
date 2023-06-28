f7adef7e5b0cc6262a71a21031769f4087a7a79a
Maurício Braz Gonçalves <82123955+mauriciobraz@users.noreply.github.com>
2023-06-28 01:01:30 -0400

    feat: create contact component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Contact from '@/components/contact.component';
 import Footer from '@/components/footer.component';
 import About from '@/components/layouts/about.layout';
 import Landing from '@/components/layouts/landing.layout';
@@ -22,6 +23,7 @@ export default function Home() {
       </div>
 
       <Services />
+      <Contact />
       <Footer />
     </main>
   );
